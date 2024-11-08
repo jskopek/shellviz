d83ea9a4fc92f1efe8357ae2dc0bb376a276958e
Jean-Marc Skopek <jskopek@gmail.com>
2024-11-08 08:25:10 +0800

    Server tracks pending entries that have not yet been sent to client via websocket; can `wait()` until all messages are sent

--- server.py
+++ server.py
@@ -8,11 +8,12 @@ from utils_html import parse_request, write_404, write_file, get_local_ip, print
 from utils_websockets import send_websocket_message, receive_websocket_message, perform_websocket_handshake
 
 class Server:
-    def __init__(self, value=None, port=5544, host="0.0.0.0"):
-        self.entries = []
-        self._initial_value = value # if an initial value is provided, it will be visualized once the server is started
+    def __init__(self, port=5544, show_url=True):
+        self.entries = []  # store a list of all existing entries; client will show these entries on page load
+        self.pending_entries = []  # store a list of all pending entries that have yet to be sent via websocket connection
 
-        self.host = host
+        self.show_url = show_url # whether to show the server's URL in the console
+
         self.port = port
 
         self.loop = asyncio.new_event_loop() # the event loop that is attached to the thread created for this instance; new `create_task` async methods are added to the loop
@@ -33,16 +34,11 @@ class Server:
         threading.Thread(target=self._run_event_loop, daemon=True).start()  # Run loop in background thread; daemon=True ensures that the thread is killed when the main thread exits
 
     def _run_event_loop(self):
-        # run initial visualization command if initial entries is provided
-        if self._initial_value:
-            self.visualize(self._initial_value)
-            self._initial_value = None
-
         asyncio.set_event_loop(self.loop) # set this thread's event loop to the main event loop
         self.loop.run_forever() # keep the event loop running
 
     def shutdown(self):
-        print("Shutting down server...")
+        # print("Shutting down server...")
 
         # shuts down the http and websocket servers
         if self.http_server_task:
@@ -68,14 +64,10 @@ class Server:
 
     # -- HTTP sever methods --
     async def start_http_server(self):
-        server = await asyncio.start_server(self.handle_http, self.host, self.port)  # start the tcp server on the specified host and port
-        print(f'Serving on http://{get_local_ip()}:{self.port}')
+        server = await asyncio.start_server(self.handle_http, '0.0.0.0', self.port)  # start the tcp server on the specified host and port
 
-        try:
-            # if qrcode module is installed, output a QR code with the server's URL; fail silently if the package is not included
-            print_qr(f'http://{get_local_ip()}:{self.port}')
-        except ImportError:
-            pass
+        if self.show_url:
+            self.print_url()
 
         async with server:
             await server.serve_forever() # server will run indefinitely until the method's task is `.cancel()`ed
@@ -92,8 +84,9 @@ class Server:
 
     # -- WebSocket server methods --
     async def start_websocket_server(self):
-        server = await asyncio.start_server(self.handle_websocket_connection, self.host, self.port + 1)
-        print(f'Serving on ws://{self.host}:{self.port + 1}')
+        server = await asyncio.start_server(self.handle_websocket_connection, '0.0.0.0', self.port + 1)
+
+        # print(f'Serving on ws://{get_local_ip()}:{self.port + 1}')
 
         async with server:
             await server.serve_forever()  # server will run indefinitely until the method's task is `.cancel()`ed
@@ -103,6 +96,10 @@ class Server:
         await perform_websocket_handshake(reader, writer)
 
         self.websocket_clients.add(writer)
+
+        # send any pending updates to clients via websocket
+        asyncio.run_coroutine_threadsafe(self.send_pending_entries_to_websocket_clients(), self.loop)
+
         try:
             while True:
                 message = await receive_websocket_message(reader)
@@ -117,14 +114,19 @@ class Server:
             writer.close()
             await writer.wait_closed()
 
-    async def message_websocket_clients(self, entry):
-        value = json.dumps(entry)
-        for writer in self.websocket_clients:
-            await send_websocket_message(writer, value)
+    async def send_pending_entries_to_websocket_clients(self):
+        if not self.websocket_clients:
+            return # No clients to send to
+
+        while self.pending_entries:
+            entry = self.pending_entries.pop(0)
+            value = json.dumps(entry)
+            for writer in self.websocket_clients:
+                await send_websocket_message(writer, value)
 
     # -- / WebSocket server methods --
 
-    def visualize(self, value, id: str = None):
+    def send(self, value, id: str = None, wait: bool=False):
         # wrap data in a dictionary with an id
         entry = {
             'id': id or str(time.time()),
@@ -139,32 +141,53 @@ class Server:
         else:
             self.entries.append(entry)
 
-        # send new entry to all clients via websocket
-        asyncio.run_coroutine_threadsafe(self.message_websocket_clients(entry), self.loop)
+        # add to list of pending entries that should be sent the client via websocket
+        self.pending_entries.append(entry)
+
+        # send pending entries to all clients via websocket
+        asyncio.run_coroutine_threadsafe(self.send_pending_entries_to_websocket_clients(), self.loop)
+
+        if wait:
+            self.wait()
+    
+    def wait(self):
+        while self.pending_entries:
+            time.sleep(0.1)
+        
+    def print_url(self):
+        print(f'Serving on http://{get_local_ip()}:{self.port}')
+
+        try:
+            # if qrcode module is installed, output a QR code with the server's URL; fail silently if the package is not included
+            print_qr(f'http://{get_local_ip()}:{self.port}')
+        except ImportError:
+            pass
+
 
 
 # Usage example
 if __name__ == "__main__":
     # Start server instance
-    s = Server()
+    # s = Server()
 
     # while True:
     #     try:
     #         content = input("Enter content: ")
-    #         s.visualize(content)
+    #         s.send(content)
     #     except KeyboardInterrupt:
     #         break
 
+    # import random
     # i = 0
     # while True:
-    #     s.visualize(f'message {i}')
-    #     s2.visualize(f'another server message {i}')
+    #     s.send(f'message {i}')
+    #     s.send(random.random(), id='progress')
+    #     # s2.send(f'another server message {i}')
     #     i += 1
-    #     time.sleep(3)
+    #     time.sleep(5)
 
 
-    time.sleep(2)
-    s.visualize('welcome to the server')
-    time.sleep(2)
-    s.visualize('another message')
-    time.sleep(2)
+    s = Server()
+    s.send('another message')
+    s.send('a third message')
+    s.wait()
