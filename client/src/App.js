81cead0976caca7fb05587d29792baffe239b7d0
Jean-Marc Skopek <jskopek@gmail.com>
2024-11-22 13:39:48 +0800

    Client attempts to connect to default websocket when dev mode

--- client/src/App.js
+++ client/src/App.js
@@ -26,7 +26,11 @@ function App() {
 		const connectWebSocket = () => {
 			setStatus('connecting');
 			// console.log('Attempting to connect to WebSocket');
-			const port = parseInt(new URLSearchParams(window.location.search).get('port')) || (parseInt(window.location.port) + 1 || 5545)
+			let port = parseInt(new URLSearchParams(window.location.search).get('port')) || (parseInt(window.location.port) + 1 || 5545)
+			if(window.location.port === '3000') {
+				console.warn('Development port detected (3000). Using default websocket port 5545. To override, add ?port=XXXX to the URL');
+				port = 5545;
+			}
 			ws = new WebSocket("ws://" + window.location.hostname + ":" + port);
 
 			ws.onopen = function () {
